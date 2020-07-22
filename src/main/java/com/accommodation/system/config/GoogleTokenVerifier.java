4a6043693d30b3c7e022f73bfc3f7b9201c3dc34
quanghuongg <thelove2505>
2020-07-23 00:33:53 +0700

    client_id

--- src/main/java/com/accommodation/system/config/GoogleTokenVerifier.java
+++ src/main/java/com/accommodation/system/config/GoogleTokenVerifier.java
@@ -12,15 +12,16 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 @Component
 public class GoogleTokenVerifier {
 
     private static final HttpTransport transport = new NetHttpTransport();
     private static final JsonFactory jsonFactory = new JacksonFactory();
-    private static final String CLIENT_ID = "649195066861-vmuch8aoo3eaqhl2qrqpje5j08jpfsu2.apps.googleusercontent.com";
+    private static final String CLIENT_ID = "649195066861-l3kdb6478u61317jvt0vrn9qdaspufjo.apps.googleusercontent.com";
 
 
     public Payload verify(String idTokenString)
@@ -30,10 +31,15 @@ public class GoogleTokenVerifier {
 
     private static Payload verifyToken(String idTokenString)
             throws GeneralSecurityException, IOException, ApiServiceException {
+        List<String> clientIds = new ArrayList<>();
+        clientIds.add(CLIENT_ID);
+        clientIds.add("649195066861-jko1msk8gr23dq6f1oh2qtkqfojqalds.apps.googleusercontent.com");
+        clientIds.add("649195066861-vmuch8aoo3eaqhl2qrqpje5j08jpfsu2.apps.googleusercontent.com");
+
         final GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.
                 Builder(transport, jsonFactory)
                 .setIssuers(Arrays.asList("https://accounts.google.com", "accounts.google.com"))
-                .setAudience(Collections.singletonList(CLIENT_ID))
+                .setAudience(clientIds)
                 .build();
 
         System.out.println("validating:" + idTokenString);
